3301279177fa62b410137c870f45ac6a000c0b4e
sarveshwani0501 <smwsarvesh@gmail.com>
2025-04-26 16:47:17 +0530

    Update MongoURL connection

--- connection.js
+++ connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 function connectToMongoose(url) {
-  return mongoose.connect(url);
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected successfully 🚀"))
+    .catch((err) => console.error("MongoDB connection error: ", err));
 }
 
 module.exports = {
